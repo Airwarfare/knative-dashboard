0c02a1cc372b37e6ca8282705fdf61f938173f14
Jordan L <12879278+Airwarfare@users.noreply.github.com>
2022-05-03 04:56:26 -0500

    Add resolver test

--- service/src/services/HelloWorldService.spec.ts
+++ service/src/services/HelloWorldService.spec.ts
@@ -1,10 +1,26 @@
 import "reflect-metadata"
+import createTestServer from "../../test/utils/server"
 import container from "../container"
 import TYPES from "../types"
 import { HelloWorldService } from "./HelloWorldService"
+
 
 test(`it returns 'world'`, () => {
     const value = container.get<HelloWorldService>(TYPES.HelloWorldService).hello()
 
     expect(value).toBe("world")
 })
+
+test(`it returns 'world' from resovler`, async () => {
+    const testServer = createTestServer();
+
+    const query = `query {
+        hello
+    }`;
+    const result = await testServer.executeOperation({
+        query
+    })
+    expect(result.errors).toBeUndefined();
+    expect(result.data).not.toBeNull();
+    expect(result.data?.hello).toBe("world");
+});
